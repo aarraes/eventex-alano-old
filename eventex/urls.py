bdee5ebd1698a5df25a52493f65dabb954030e8b
Alano Arraes <alano.arraes@actiotecnologia.com.br>
2013-07-26 17:52:54 -0400

    All your base are belong to us 2

--- eventex/urls.py
+++ eventex/urls.py
@@ -4,7 +4,7 @@ urlpatterns = patterns('eventex.core.views',
     url(r'^$', 'homepage', name='home'),
 )
 from django.conf import settings
-    urlpatterns += patterns('django.views.static',
-	    url(r'^static/(?P<path>.*)$', 'serve',
-		    {'document_root': settings.STATIC_ROOT}),
+urlpatterns += patterns('django.views.static',
+    url(r'^static/(?P<path>.*)$', 'serve',
+        {'document_root': settings.STATIC_ROOT}),
 )
